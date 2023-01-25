776c2e7cffedda066039bde1053568c3f2cdff34
Arash Zhianfard <arash.zhianfard@gmail.com>
2023-01-25 16:37:38 +0100

    feat:Payment dbcontext addded

---
+++ PaymentService/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rebus_simple_project.Models;
+
+namespace Rebus_simple_project
+{
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "AuthorDb");
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
