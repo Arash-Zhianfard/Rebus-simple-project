94cf7406295467df2b993d8c71d5da48bc0548a9
Arash Zhianfard <arash.zhianfard@gmail.com>
2023-01-25 16:35:18 +0100

    feat:EF added

---
+++ PaymentService/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿//using System.Collections.Generic;
+
+//namespace Rebus_simple_project
+//{
+//    public class ApiContext : DbContext
+//    {
+//        protected override void OnConfiguring
+//       (DbContextOptionsBuilder optionsBuilder)
+//        {
+//            optionsBuilder.UseInMemoryDatabase(databaseName: "AuthorDb");
+//        }
+//        public DbSet<Author> Authors { get; set; }
+//        public DbSet<Book> Books { get; set; }
+//    }
+//}
