eb475754863036fbabdf532cc34775871dc81ea0
Arash Zhianfard <arash.zhianfard@gmail.com>
2023-03-03 11:26:15 +0100

    fix: change project type from lib to exe

---
+++ Common/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Topper;
+
+namespace Common
+{
+    static class Program
+    {
+        private static IConfiguration Configuration { get; set; }
+
+        static void Main()
+        {
+            Log.Logger = new LoggerConfiguration()
+               .WriteTo.Console()
+               .CreateLogger();
+
+            ReadConfiguration();
+
+            var serviceConfiguration = new ServiceConfiguration()
+               .Add("OurBackendBus", () => new RebusConfig(Configuration));
+
+            ServiceHost.Run(serviceConfiguration);
+        }
+
+        private static void ReadConfiguration()
+        {
+            var builder =
+                new ConfigurationBuilder();                   
+            Configuration = builder.Build();
+        }
+    }
+}
